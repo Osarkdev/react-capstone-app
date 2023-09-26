e7ea753877a8f10fd96bb854bce037621547215f
Osarkdev <81278958+Osarkdev@users.noreply.github.com>
2023-09-25 20:54:46 -0600

    unit test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import BookingForm from './components/booking/BookingForm';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Render Label for booking date', () => {
+  render(<BookingForm/>);
+  const labelElement = screen.getByText('Choose Time');
+  expect(labelElement).toBeInTheDocument();
+})
